cd0c2ab5f8be875a985fe3e2d96ea88882696e7b
DakoMumladze <dako.dako.mumladze@gmail.com>
2020-11-03 01:18:08 +0400

    Added Icon component

---
+++ src/components/Icon/Circle.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Plus = () => {
+  return (
+  <svg xmlns="http://www.w3.org/2000/svg"
+   width="32.809"
+   height="23.456"
+   viewBox="0 0 32.809 23.456">
+    <g id="rendom" transform="translate(0.621 0.032)" opacity="0.3">
+      <path id="Path_3" data-name="Path 3"
+      d="M7.072,17.348A10.178,10.178,0,0,0,24.76,8.658M22.427,3.963A10.182,10.182,0,0,0,4.783,12.632"
+      transform="translate(1 1)"
+      fill="none" stroke="#4b4b4f"
+      strokeLinecap="round" strokeWidth="3" fillRule="evenodd"/>
+      <path id="Path_4" data-name="Path 4"
+      d="M20.148,12.925l4.459-4.459,4.459,4.459M9.418,8.466,4.96,12.925.5,8.466"
+      transform="translate(1 1)" fill="none" stroke="#4b4b4f"
+      strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" fillRule="evenodd"/>
+    </g>
+  </svg>
+  )
+}
+
+export default Plus
