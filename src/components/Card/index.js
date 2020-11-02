50aa2ddc23ef931ec32f7e1de669398d66daf3fa
DakoMumladze <dako.dako.mumladze@gmail.com>
2020-11-03 01:18:20 +0400

    Added card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import { StyledCard } from './style'
+
+const Header = () => {
+  return (
+      <StyledCard>
+        dsdsd
+      </StyledCard>
+  )
+}
+
+export default Header
